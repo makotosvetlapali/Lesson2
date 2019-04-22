0daa4a03912d83336cb03117c60ea4801f5f899b
makotosvetlapali <donpedjas@gmail.com>
2019-04-22 18:12:40 +0200

    Commit 1

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -1,13 +1,21 @@
 package optional;
 
-/* 
- * Print the lyrics to the song "99 Bottles of Beer on the wall"
- * 
- * http://99-bottles-of-beer.net/lyrics.html
- */
 
 public class BeerOnTheWall {
 	public static void main(String[] args) {
+		
+		for (int i = 99; i > 0; i--) {
+			
+			System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+			System.out.println("Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+			System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+			System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");
+			
+		
+
+			
+
 
 	}
+}
 }
